9a7e52624ce72b092f2199283cf23e5ca2ba2f5d
Leila Methnani <leilamethnani@gmail.com>
2016-01-27 00:26:37 -0800

    included animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $('.toggle').click(function (event) {
 	event.preventDefault();
 	var target = $(this).attr('href');
-	$(target).toggle('bounce');
+	$(target).toggle('bounce', 600);
+
+
+	var options = {
+      duration: 500,
+      easing: 'linear'
+   };
+	$('.profile').animate({
+		right: '100px'}, options).animate({
+		opacity: 0.25,
+		left:'100%'}, options);
 });
