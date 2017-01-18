cfedb7f1203d2cf415ace934f36c5c99ff35f37a
Leila Methnani <leilamethnani@gmail.com>
2017-01-19 00:33:24 +0400

    webpage REVAMP

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 $('.toggle').click(function (event) {
 	event.preventDefault();
+	//var target = $(this).attr('href');
+	//$(target).toggle('bounce', 600);
 	var target = $(this).attr('href');
-	$(target).toggle('bounce', 600);
 
 
 	var options = {
       duration: 500,
       easing: 'linear'
    };
+
 	$('.profile').animate({
 		right: '100px'}, options).animate({
-		left:'150%'}, options).fadeOut(500);
+			left:'150%'}, options).fadeOut(500, function(){
+				window.location = target;
+				//location.reload(); 
+
+});
 });
