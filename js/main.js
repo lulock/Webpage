f96e10e127936fff230757eae2df3a30169ad030
Leila Methnani <leilamethnani@gmail.com>
2017-01-20 20:44:28 +0400

    blender gallery is better

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 $('.toggle').click(function (event) {
-	event.preventDefault();
-	//var target = $(this).attr('href');
-	//$(target).toggle('bounce', 600);
-	var target = $(this).attr('href');
+    event.preventDefault();
+    //var target = $(this).attr('href');
+    //$(target).toggle('bounce', 600);
+    var target = $(this).attr('href');
 
 
-	var options = {
-      duration: 500,
-      easing: 'linear'
-   };
+    var options = {
+        duration: 500,
+        easing: 'linear'
+    };
 
-	$('.profile').animate({
-		right: '100px'}, options).animate({
-			left:'150%'}, options).fadeOut(500, function(){
-				window.location = target;
-				//location.reload(); 
+    $('.profile').animate({
+        right: '100px'}, options).animate({
+        left:'150%'}, options).fadeOut(500, function(){
+        window.location = target;
+        //location.reload(); 
 
-});
+    });
 });
