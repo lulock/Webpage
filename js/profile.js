3609f92a0bfaef7c43e37b5ce611aeca7eff88f2
Leila Methnani <leilamethnani@gmail.com>
2017-01-22 16:31:04 +0400

    added next and prev navigation to blender gallery

--- js/profile.js
+++ js/profile.js
@@ -15,7 +15,7 @@ $(function(){
    $('a.more').click(function(){
      $('#overlay:not(:animated)')
        .addClass('active')
-       .html('<p>Hey! My name is Leila and I am a computer scientist. I try to be creative, so my interests reach far and wide. You will find me dabbling with different things every so often. Right now, I am super excited by 3D graphic art, so have been learning a lot about it. My current CS focus is in graphics and animation, so if you would like to collaborate on something fun (games included), then hit me up on e-mail or find me on github. Otherwise, this is a little peak into my world. Enjoy!</p>')
+       .html('<p>Hey! My name is Leila and I am a computer scientist. I try to be creative, so my interests reach far and wide. You will find me dabbling with different things every so often. Right now, I am super excited by 3D graphic art, so I have been learning a lot about it. My current CS focus is in graphics and animation, so if you would like to collaborate on something fun (games included), then hit me up on e-mail or find me on github. Otherwise, this is a little peak into my world. Enjoy!</p>')
        .slideDown();
    });
  
